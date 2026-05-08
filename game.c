110a03852a0de2ae03f8d0244df73d0341de3f12
sjohnson399-dot <sjohnson399@toromail.csudh.edu>
2026-05-07 18:15:37 -0700

    had to fix my indents

--- game.c
+++ game.c
@@ -3321,20 +3321,20 @@ void stanPush(void){
 
         int stanVisits[5] = {0}, stanChoice, stanRandy;
 
-          do {
+        do {
 
-        printf("\n=== stanley's room ===\n");
+        	printf("\n=== stanley's room ===\n");
 
-        for(int i = 0; i < 5; i++) {
-            printf("%d. Enter %s\n", i + 1, stanRooms[i]);
-        }
+        	for(int i = 0; i < 5; i++) {
+            	printf("%d. Enter %s\n", i + 1, stanRooms[i]);
+        	}
 
-        printf("6. quit\n");
+        	printf("6. quit\n");
 
-        printf("choose a room: ");
-        scanf("%d", &stanChoice);
+        	printf("choose a room: ");
+        	scanf("%d", &stanChoice);
 
-        if(stanChoice >= 1 && stanChoice <= 5) {
+        	if(stanChoice >= 1 && stanChoice <= 5) {
 
             stanVisits[stanChoice - 1]++;
 
@@ -3353,25 +3353,25 @@ void stanPush(void){
                 printf("nothing happened\n");
             }
 
-        }
-        else if(stanChoice == 6) {
+        	}
+        	else if(stanChoice == 6) {
 
-            printf("\n=== rooms visits ===\n");
+            	printf("\n=== rooms visits ===\n");
 
-            for(int i = 0; i < 5; i++) {
+            	for(int i = 0; i < 5; i++) {
 
-                printf("%s visited %d time(s)\n",
-                       stanRooms[i],
-                       stanVisits[i]);
-            }
+                	printf("%s visited %d time(s)\n",
+                       	stanRooms[i],
+                       	stanVisits[i]);
+            	}
 
-            printf("goodbye\n");
-        }
-        else {
+            	printf("goodbye\n");
+        	}
+        	else {
 
-            printf("invalid choice\n");
-        }
+            	printf("invalid choice\n");
+        	}
 
-    } while(stanChoice != 6);
+    	} while(stanChoice != 6);
 
 }
